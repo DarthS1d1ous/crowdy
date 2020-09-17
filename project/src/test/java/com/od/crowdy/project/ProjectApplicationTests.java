a8a4a30b5165d59cac80c01fc115ffd6dbb125ba
plusqRED <rickes.oleg@gmail.com>
2020-09-17 15:28:17 +0300

    initial commit

---
+++ project/src/test/java/com/od/crowdy/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.od.crowdy.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
