72e921e821b0f42f27bb973da157b70b4545585c
plusqRED <rickes.oleg@gmail.com>
2020-09-17 16:06:53 +0300

    mongodb cloud connection

--- project/src/main/java/com/od/crowdy/project/ProjectApplication.java
+++ project/src/main/java/com/od/crowdy/project/ProjectApplication.java
@@ -1,13 +1,24 @@
 package com.od.crowdy.project;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
+@Log4j2
 public class ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void serviceStarted() {
+		log.info("Server has started! {}", LocalDateTime.now());
 	}
 
 }
