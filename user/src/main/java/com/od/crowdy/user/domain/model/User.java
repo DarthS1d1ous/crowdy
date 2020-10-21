b1823e98b13b2806537d06e5aa0b5ce26570408d
plusqRED <rickes.oleg@gmail.com>
2020-10-21 22:27:37 +0300

    Default mongodb incremented id has been deleted by reason of Google OAuth2

--- user/src/main/java/com/od/crowdy/user/domain/model/User.java
+++ user/src/main/java/com/od/crowdy/user/domain/model/User.java
@@ -1,13 +1,7 @@
 package com.od.crowdy.user.domain.model;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("user")
 public class User extends com.od.crowdy.shared.domain.model.User {
-    @Id
-    @Override
-    public String getId() {
-        return super.getId();
-    }
 }
